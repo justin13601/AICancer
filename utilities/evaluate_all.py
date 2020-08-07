2de43dd1d83aac4a907d77db99a72dfdf1824fdc
Justin Xu <52216145+justin13601@users.noreply.github.com>
2020-08-07 01:48:37 -0400

    Create evaluate_all.py

---
+++ utilities/evaluate_all.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from PIL import *
+from torchvision import datasets, models, transforms
+from torchsummary import summary
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.autograd import Variable
+
+def evaluate_all(net, loader, criterion):
+    """ 
+    Evaluates the network on a dataset.
+
+    Args:
+        net: PyTorch neural network object
+        loader: PyTorch data loader for the validation set
+        criterion: The loss function
+    Returns:
+        err: A scalar for the avg classification error over the validation set
+        loss: A scalar for the average loss function over the validation set
+     """
+    total_loss = 0.0
+    total_err = 0.0
+    total_epoch = 0
+    for i, data in enumerate(loader, 0):
+        inputs, labels = data
+        # Convert labels to 0/1
+        #newlabels=torch.tensor([])
+        #print(type(labels))
+        #for i in labels:
+              #if i < 2:
+              #  newlabels=torch.cat((newlabels,torch.tensor([0]).float()),0)
+              #else:
+                #newlabels=torch.cat((newlabels,torch.tensor([1]).float()),0)
+        if use_cuda and torch.cuda.is_available():
+            inputs= inputs.cuda()
+            labels = labels.cuda()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels.float())
+        corr = (outputs > 0.0).squeeze().long() != labels
+        total_err += int(corr.sum())
+        total_loss += loss.item()
+        total_epoch += len(labels)
+    err = float(total_err) / total_epoch
+    loss = float(total_loss) / (i + 1)
+    return err, loss
+    
