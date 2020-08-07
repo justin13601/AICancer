a8a65338d0be94c10f96b775f376036403654876
Justin Xu <52216145+justin13601@users.noreply.github.com>
2020-08-07 01:51:14 -0400

    Update train_net.py

--- utilities/train_net.py
+++ utilities/train_net.py
@@ -14,49 +14,31 @@ from torchsummary import summary
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.autograd import Variable
 
-#Training Function (adapted from Lab 2)
-def train_net(net, train_loader, val_loader, batch_size=150, learning_rate=0.005, num_epochs=6):
-    # Define the Loss function and optimizer
-    # The loss function will be Binary Cross Entropy (BCE). In this case we
-    # will use the BCEWithLogitsLoss which takes unnormalized output from
-    # the neural network and scalar label.
-    # Optimizer will be SGD with Momentum.
-  
+
+def train_net(net, train_loader, val_loader, batch_size=150, learning_rate=0.005, num_epochs=6):  
     criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
-    ########################################################################
-    # Set up some numpy arrays to store the training/test loss/erruracy
     train_err = np.zeros(num_epochs)
     train_loss = np.zeros(num_epochs)
     val_err = np.zeros(num_epochs)
     val_loss = np.zeros(num_epochs)
-    ########################################################################
-    # Train the network
-    # Loop over the data iterator and sample a new batch of training data
-    # Get the output from the network, and optimize our loss function.
     start_time = time.time()
 
-    for epoch in range(num_epochs):  # loop over the dataset multiple times
+    for epoch in range(num_epochs):
         total_train_loss = 0.0
         total_train_err = 0.0
         total_epoch = 0
         i=0
         for imgs, labels in iter(train_loader):
-            labels = normalize_label(labels) # Convert labels to 0/1
+            labels = normalize_label(labels)
             if use_cuda and torch.cuda.is_available():
               imgs = imgs.cuda()
               labels = labels.cuda()
-            # Zero the parameter gradients
             optimizer.zero_grad()
-            #print(imgs.shape)
-            # Forward pass, backward pass, and optimize
             outputs = net(imgs)
-            #print(labels.float())
-            #print(outputs.shape)
             loss = criterion(outputs, labels.float())
             loss.backward()
             optimizer.step()
-            # Calculate the statistics
             corr = (outputs > 0.0).squeeze().long() != labels
             total_train_err += int(corr.sum())
             total_train_loss += loss.item()
@@ -72,14 +54,12 @@ def train_net(net, train_loader, val_loader, batch_size=150, learning_rate=0.005
                    train_loss[epoch],
                    val_err[epoch],
                    val_loss[epoch]))
-        # Save the current model (checkpoint) to a file
         model_path = get_model_name(net.name, batch_size, learning_rate, epoch)
         torch.save(net.state_dict(), model_path)
     print('Finished Training')
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Total time elapsed: {:.2f} seconds".format(elapsed_time))
-    # Write the train/test loss/err into CSV file for plotting later
     epochs = np.arange(1, num_epochs + 1)
     np.savetxt("{}_train_err.csv".format(model_path), train_err)
     np.savetxt("{}_train_loss.csv".format(model_path), train_loss)
