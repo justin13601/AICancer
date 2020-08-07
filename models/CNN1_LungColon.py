fbcaaa77845e293315e696193c31bd1c2350cf9d
Justin Xu <52216145+justin13601@users.noreply.github.com>
2020-08-07 16:33:41 -0400

    Update CNN1_LungColon.py

--- models/CNN1_LungColon.py
+++ models/CNN1_LungColon.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
+
 class CNN1_LungColon(nn.Module):
     def __init__(self):
         super(CNN1_LungColon, self).__init__()
@@ -14,13 +15,13 @@ class CNN1_LungColon(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(5, 10, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(10*53*53, 32)
+        self.fc1 = nn.Linear(10 * 53 * 53, 32)
         self.fc2 = nn.Linear(32, 1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 10*53*53)
+        x = x.view(-1, 10 * 53 * 53)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         x = x.squeeze(1)
