d89da3de2b5a1a5d43909959f79f2208eea8e7d7
Justin Xu <52216145+justin13601@users.noreply.github.com>
2020-08-07 16:35:24 -0400

    Update predict_funcs.py

--- utilities/predict_funcs.py
+++ utilities/predict_funcs.py
@@ -1,3 +1,4 @@
+import PIL.Image
 import numpy as np
 import os
 import time
@@ -12,69 +13,86 @@ from PIL import *
 from torchvision import datasets, models, transforms
 from torchsummary import summary
 from torch.utils.data.sampler import SubsetRandomSampler
+from PIL import *
 from torch.autograd import Variable
 
+import models.CNN2_LungClassifier
+import utilities.utility_funcs
 
-def all_models(data,CNN1,CNN2,CNN3,CNN4):
-  for images,labels in data:
-    for element, lab in zip(images,labels):
-      res=""
-      element=element.unsqueeze(0)
-      out1=torch.sigmoid(CNN1(element.cuda()))
-      #print(out1, lab)
-      if out1[0]>0.5:
-        #res=("Lung")
-        out2=torch.sigmoid(CNN2(element.cuda()))
-        if out2[0]<0.5:
-          res= "Lung: benign"
+
+def all_models(data, CNN1, CNN2, CNN3, CNN4, use_cuda=False):
+    for images, labels in data:
+        for element, lab in zip(images, labels):
+            res = ""
+            element = element.unsqueeze(0)
+            if use_cuda:
+                out1 = torch.sigmoid(CNN1(element.cuda()))
+            else:
+                out1 = torch.sigmoid(CNN1(element))
+            if out1[0] > 0.5:
+                if use_cuda:
+                    out2 = torch.sigmoid(CNN2(element.cuda()))
+                else:
+                    out2 = torch.sigmoid(CNN2(element))
+                if out2[0] < 0.5:
+                    res = "Lung: Benign"
+                else:
+                    if use_cuda:
+                        out4 = torch.sigmoid(CNN4(element.cuda()))
+                    else:
+                        out4 = torch.sigmoid(CNN4(element))
+                    if out4[0] < 0.5:
+                        res = "Lung: Malignant - ACA"
+                    else:
+                        res = "Lung: Malignant - SCC"
+            else:
+                if use_cuda:
+                    out3 = torch.sigmoid(CNN3(element.cuda()))
+                else:
+                    out3 = torch.sigmoid(CNN3(element))
+                if out3[0] < 0.5:
+                    res = "Colon: Benign"
+                else:
+                    res = "Colon: Malignant"
+            print(res)
+
+
+def classify_image(img_path, CNN1, CNN2, CNN3, CNN4, use_cuda=False):
+    element = image_loader(img_path)
+    res = ""
+    if use_cuda:
+        out1 = torch.sigmoid(CNN1(element.cuda()))
+    else:
+        out1 = torch.sigmoid(CNN1(element))
+    if out1[0] > 0.5:
+        if use_cuda:
+            out2 = torch.sigmoid(CNN2(element.cuda()))
         else:
-          #res = "Lung: malignant"
-          out4=torch.sigmoid(CNN4(element.cuda()))
-          #print(out4[0],lab)
-          if out4[0]<0.5:
-            res = "Lung: malignant: aca"
-          else:
-           res = "Lung: malignant: scc"
-      else:
-        #print("colon")
-        out3=torch.sigmoid(CNN3(element.cuda()))
-        if out3[0]<0.5:
-          res = "Colon: benign"
+            out2 = torch.sigmoid(CNN2(element))
+        if out2[0] < 0.5:
+            res = "Lung: Benign"
         else:
-          res = "Colon: malignant"
-      print(res)
-      
-      
-def single_image_checker(img_path,CNN1,CNN2,CNN3,CNN4):
-      element = image_loader(sample_image_path)
-      res=""
-      element=element.unsqueeze(0)
-      out1=torch.sigmoid(CNN1(element.cuda()))
-      #print(out1, lab)
-      if out1[0]<1:
-        #res=("Lung")
-        out2=torch.sigmoid(CNN2(element.cuda()))
-        if out2[0]<0.5:
-          res= "Lung: benign"
+            if use_cuda:
+                out4 = torch.sigmoid(CNN4(element.cuda()))
+            else:
+                out4 = torch.sigmoid(CNN4(element))
+            if out4[0] < 0.5:
+                res = "Lung: Malignant - ACA"
+            else:
+                res = "Lung: Malignant - SCC"
+    else:
+        if use_cuda:
+            out3 = torch.sigmoid(CNN3(element.cuda()))
         else:
-          #res = "Lung: malignant"
-          out4=torch.sigmoid(CNN4(element.cuda()))
-          #print(out4[0],lab)
-          if out4[0]<0.5:
-            res = "Lung: malignant: aca"
-          else:
-           res = "Lung: malignant: scc"
-      else:
-        #print("colon")
-        out3=torch.sigmoid(CNN3(element.cuda()))
-        if out3[0]<0.5:
-          res = "Colon: benign"
+            out3 = torch.sigmoid(CNN3(element))
+        if out3[0] > 0.5:
+            res = "Colon: Benign"
         else:
-          res = "Colon: malignant"
-      print(res)
-      
-      
-def image_loader(image_name):
+            res = "Colon: Malignant"
+    print(res)
+
+
+def image_loader(image_name, use_cuda=False):
     """
     Load image and prepares it for model.
     
@@ -83,14 +101,20 @@ def image_loader(image_name):
     Returns:
         image.cuda: CUDA tensor
     """
-    image = Image.open(image_name)
+    image = PIL.Image.open(image_name)
+    data_transform2 = transforms.Compose([transforms.Resize((224, 224)),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     image = data_transform2(image).float()
     image = image.unsqueeze(0)
     image = Variable(image)
-    return image.cuda()
+    if use_cuda:
+        return image.cuda()
+    else:
+        return image
 
-  
-def predict(net, model_path, sample_image_path):
+
+def predict(net, model_path, sample_image_path, use_cuda=False):
     """
     Predicts output given a trained neural net model and an image.
 
@@ -102,8 +126,8 @@ def predict(net, model_path, sample_image_path):
     Returns:
         tensor: CUDA tensor object
 
-    >>> net = CNN2_LungClassifier()
-    >>> model_path = get_model_name(net.name, batch_size=150, learning_rate=0.01, epoch=9)
+    >>> net = models.CNN2_LungClassifier.CNN2_LungClassifier()
+    >>> model_path = utilities.utility_funcs.get_model_name(net.name, batch_size=150, learning_rate=0.01, epoch=9)
     >>> img_path = '/content/CNN2_LungClassifierData/test/benign/lungn1019.jpeg'
     >>> predict(net, model_path, img_path)
 
@@ -112,71 +136,86 @@ def predict(net, model_path, sample_image_path):
     """
     state = torch.load(model_path)
     net.load_state_dict(state)
-    net = net.cuda()
+    if use_cuda:
+        net = net.cuda()
     img = image_loader(sample_image_path)
     x = net(img)
     return torch.sigmoid(x)
-  
-  
-def Model_Loader(net, model_path):
-    state=torch.load(model_path)
+
+
+def model_loader(net, model_path, use_cuda=False):
+    if not use_cuda:
+        state = torch.load(model_path, map_location=torch.device('cpu'))
+    else:
+        state = torch.load(model_path)
     net.load_state_dict(state)
-    net = net.cuda()
+    if use_cuda:
+        net = net.cuda()
     return net
-  
-  
-def connect_accuracy(data,CNN1,CNN2,CNN3,CNN4,print_img=False):
-  total=0
-  corr=0
-  false_n_p=np.array([["pred |label","ca","cn","la","ln","ls"],
-                    ["colon_aca", 0, 0, 0, 0, 0],
-                    ["colon_n. ", 0, 0, 0, 0, 0],
-                    ["lung_aca ", 0, 0, 0, 0, 0],
-                    ["lung_n.  ", 0, 0, 0, 0, 0],
-                    ["lung_scc ", 0, 0, 0, 0, 0]
-                    ])
 
-  for images,labels in data:
-    for element, lab in zip(images,labels):
-      #print("Lab: "+str(lab))
-      lab=int(lab)
-      label_string=''
-      pred_string=''
-      if print_img==True:  
-        p=element.numpy()
-        print_plot=plt.figure()
-        p=np.transpose(p,(1,2,0))
-        plt.imshow(p)
-      classes = ['colon_aca', 'colon_n', 'lung_aca','lung_n', 'lung_scc']
-      element=element.unsqueeze(0)
-      out1=torch.sigmoid(CNN1(element.cuda()))
- 
-      if out1[0]>0.5:
-        out2=torch.sigmoid(CNN2(element.cuda()))
 
-        if out2[0]<0.5:
-          res= 3
-        else:
-          out4=torch.sigmoid(CNN4(element.cuda()))
-          if out4[0]<0.5:
-            res = 2
-          else:
-            res = 4
-      else:
-        out3=torch.sigmoid(CNN3(element.cuda()))
-        if out3[0]<0.5:
-          res = 0
-        else:
-          res = 1
+def connect_accuracy(data, CNN1, CNN2, CNN3, CNN4, print_img=False, use_cuda=False):
+    total = 0
+    corr = 0
+    false_n_p = np.array([["pred |label", "ca", "cn", "la", "ln", "ls"],
+                          ["colon_aca", 0, 0, 0, 0, 0],
+                          ["colon_n. ", 0, 0, 0, 0, 0],
+                          ["lung_aca ", 0, 0, 0, 0, 0],
+                          ["lung_n.  ", 0, 0, 0, 0, 0],
+                          ["lung_scc ", 0, 0, 0, 0, 0]
+                          ])
 
-      label_string=classes[lab]
-      pred_string=classes[res]
-      if print_img==True:
-        plt.title('Label: '+label_string+" | Pred: "+pred_string)
-      false_n_p[res+1,lab+1]=int(false_n_p[res+1,lab+1])+1
-      total+=1
-      if res==lab:
-          corr+=1
-  print(false_n_p)
-  print("correct:",corr,", total:",total,", accuracy:",corr/total)  
-  
+    for images, labels in data:
+        for element, lab in zip(images, labels):
+            lab = int(lab)
+            label_string = ''
+            pred_string = ''
+            if print_img == True:
+                p = element.numpy()
+                print_plot = plt.figure()
+                p = np.transpose(p, (1, 2, 0))
+                plt.imshow(p)
+            classes = ['colon_aca', 'colon_n', 'lung_aca', 'lung_n', 'lung_scc']
+            element = element.unsqueeze(0)
+            if use_cuda:
+                out1 = torch.sigmoid(CNN1(element.cuda()))
+            else:
+                out1 = torch.sigmoid(CNN1(element))
+
+            if out1[0] > 0.5:
+                if use_cuda:
+                    out2 = torch.sigmoid(CNN2(element.cuda()))
+                else:
+                    out2 = torch.sigmoid(CNN2(element))
+
+                if out2[0] < 0.5:
+                    res = 3
+                else:
+                    if use_cuda:
+                        out4 = torch.sigmoid(CNN4(element.cuda()))
+                    else:
+                        out4 = torch.sigmoid(CNN4(element))
+                    if out4[0] < 0.5:
+                        res = 2
+                    else:
+                        res = 4
+            else:
+                if use_cuda:
+                    out3 = torch.sigmoid(CNN3(element.cuda()))
+                else:
+                    out3 = torch.sigmoid(CNN3(element))
+                if out3[0] < 0.5:
+                    res = 0
+                else:
+                    res = 1
+
+            label_string = classes[lab]
+            pred_string = classes[res]
+            if print_img == True:
+                plt.title('Label: ' + label_string + " | Pred: " + pred_string)
+            false_n_p[res + 1, lab + 1] = int(false_n_p[res + 1, lab + 1]) + 1
+            total += 1
+            if res == lab:
+                corr += 1
+    print(false_n_p)
+    print("correct:", corr, ", total:", total, ", accuracy:", corr / total)
